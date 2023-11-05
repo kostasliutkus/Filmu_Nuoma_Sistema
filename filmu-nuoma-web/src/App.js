97a0160aabb4c66097d1af38d5b93969e9bfcb65
Clyde <klaidas.tadaravicius@gmail.com>
2023-11-05 19:43:48 +0200

    Added a link to orders

--- filmu-nuoma-web/src/App.js
+++ filmu-nuoma-web/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import FilmList from './pages/FilmSystem/FilmList.js'
 import {Link,Route,Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import OrderList from "./pages/OrderSystem/Order.js";
 function App() {
   return (
       <>
@@ -10,11 +11,13 @@ function App() {
           <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/FilmList">Films</Link></li>
+              <li><Link to="/OrderList">Orders</Link></li>
           </ul>
       </nav>
       <Routes>
           <Route path="/" element ={<HomePage/>}/>
           <Route path="/FilmList" element ={<FilmList/>}/>
+          <Route path="/OrderList" element ={<OrderList/>}/>
       </Routes>
       </>
   );
--- filmu-nuoma-web/src/pages/OrderSystem/Order.js
+++ filmu-nuoma-web/src/pages/OrderSystem/Order.js
@@ -1,3 +1,22 @@
-export default function FirstPost() {
-    return <h1>First Post</h1>;
-  }
+function OrderList() {
+  return (
+      <body>
+      <h2>HTML Table</h2>
+      <table>
+          <tr>
+              <th>Movie</th>
+              <th>Price</th>
+              <th>somethin</th>
+          </tr>
+          <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>69</td>
+              <td>hello</td>
+          </tr>
+         
+      </table>
+
+      </body>
+  );
+}
+export default OrderList
