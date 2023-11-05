4a108875134ade024d1aae0d2ebfdfcbb9cdb050
Kostas Liutkus <kostliutk2@gmail.com>
2023-11-05 19:36:09 +0200

    Created Router and example navbar

--- filmu-nuoma-web/src/App.js
+++ filmu-nuoma-web/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import React from "react";
 import FilmList from './pages/FilmSystem/FilmList.js'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Link,Route,Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
 function App() {
   return (
-      <body>
-      <div >
-          <header className="App-header">Filmų nuomos Sistema
-              <button onClick={FilmList}>Filmų sąrašas</button>
-          </header>
-      </div>
-      </body>
-
+      <>
+      <nav>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/FilmList">Films</Link></li>
+          </ul>
+      </nav>
+      <Routes>
+          <Route path="/" element ={<HomePage/>}/>
+          <Route path="/FilmList" element ={<FilmList/>}/>
+      </Routes>
+      </>
   );
 }
 export default App;
---
+++ filmu-nuoma-web/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home()
+{
+   return(
+       <body>
+       <header className="App-header">Filmų nuomos Sistema
+       </header>
+       </body> );
+}
+export default Home
