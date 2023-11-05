9c68b80500d4607c2e1d8a983bbd7b030b5f9f07
Kostas Liutkus <kostliutk2@gmail.com>
2023-11-05 18:15:17 +0200

    Added example button

--- filmu-nuoma-web/src/App.js
+++ filmu-nuoma-web/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import FilmList from './pages/FilmSystem/FilmList.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+      <div >
+          <header className="App-header">Filmų nuomos Sistema
+              <button onClick={FilmList}>Filmų sąrašas</button>
+          </header>
+      </div>
+      </body>
+
   );
 }
-
 export default App;
