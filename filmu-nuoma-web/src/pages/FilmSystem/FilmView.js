88e7b9ca12f113452c25e954102ec7ad4b77f4f6
Clyde <klaidas.tadaravicius@gmail.com>
2023-11-13 19:28:53 +0200

    added button for creating order

--- filmu-nuoma-web/src/pages/FilmSystem/FilmView.js
+++ filmu-nuoma-web/src/pages/FilmSystem/FilmView.js
@@ -1,14 +1,52 @@
 import React from "react";
 import  {useParams} from 'react-router-dom';
 import VideoPlayer  from './VideoPlayer.js'
+import './FilmStyle.css'
 const FilmView = () => {
     const {id} = useParams();
     return (
-        <div>
-            <VideoPlayer videoId={id} width="560" height="315" />
-            <div><h2>Reviews</h2></div>
-            <div><button>Reviews</button></div>
-        </div>
+        <body>
+            <div className="encased-div">
+                <div className="grid-item">
+                    <VideoPlayer videoId={id} width="560" height="315" />
+                </div>
+
+                <div className="grid-item">
+                    <table className="recommendation-table">
+                        <thead className="recommendation-thead">
+                        <tr>
+                            <th>Movie Name</th>
+                            <th>Director</th>
+                            <th>Main Actor</th>
+                            <th>Length</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Never Gonna Give You Up</td>
+                            <td>Rick Astley</td>
+                            <td>Rick Astley</td>
+                            <td>3:32</td>
+                        </tr>
+                        <tr>
+                            <td>BASE RACE INCIDENT</td>
+                            <td>Thebausffs</td>
+                            <td>Thebausffs</td>
+                            <td>15:28</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="grid-item">
+                    <div><h2>Reviews</h2></div>
+                    <div><button>Reviews</button></div>
+                </div>
+
+            </div>
+
+
+        </body>
     );
 };
 
