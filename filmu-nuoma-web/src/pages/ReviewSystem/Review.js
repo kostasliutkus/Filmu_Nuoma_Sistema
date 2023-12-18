057f1c0435500b84daa3bd9828a0b37ec312db8b
Waspirious <kestazzxc@gmail.com>
2023-12-18 21:00:30 +0200

    reviews

--- filmu-nuoma-web/src/pages/ReviewSystem/Review.js
+++ filmu-nuoma-web/src/pages/ReviewSystem/Review.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-function Review()
-{
-    return(
-        <body>
-            <Stack direction="row" spacing={2}>
-                <Avatar >H</Avatar>
-                <Stack direction="column" spacing={0.1}>
-                    <h3>Anonimas</h3>
-                    <label>20 min ago</label>
-                </Stack>
-                <Rating name="read-only" size="large" value={1} readOnly />
-            </Stack><br></br>
-            <textarea rows="6" cols="100">Cinematek ensures a top-notch streaming experience with high-quality playback and minimal buffering. The streaming platform is adaptive, adjusting to varying internet speeds without compromising on video and audio quality. The convenience of streaming directly from the website eliminates the need for external applications, offering a hassle-free movie-watching experience.</textarea>                    
-            <Stack direction="row" spacing={0.1}>
-                <Button>LIKE</Button>
-                <Button>DISLIKE</Button>
-            </Stack>
-        </body> );
+function Review() {
+  const { id } = useParams();
+
+  const [reviewData, setReviewData] = useState(null);
+
+  useEffect(() => {
+    const fetchReview = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/reviews/${id}`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setReviewData(data);
+        } else {
+          const errorMessage = await response.text();
+          alert(`Error fetching review: ${errorMessage}`);
+        }
+      } catch (error) {
+        console.error('Error fetching review:', error);
+        alert('Error fetching review. Please try again.');
+      }
+    };
+
+    fetchReview();
+  }, [id]);
+
+  if (!reviewData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>Review Details</h2>
+
+      <Stack direction="row" spacing={2}>
+        <Avatar>H</Avatar>
+        <Stack direction="column" spacing={0.1}>
+          <h3>{reviewData.kurejas}</h3>
+          <label>{/* Display the date or other relevant information */}</label>
+        </Stack>
+        <Rating name="read-only" value={reviewData.ivertinimas} readOnly />
+      </Stack>
+      <br />
+      <p>Description: {reviewData.aprasymas}</p>
+      <p>Positive: {reviewData.teigiamas}</p>
+      <p>Negative: {reviewData.neigiamas}</p>
+
+      {/* Add any other information you want to display */}
+      
+      <Button variant="contained" color="primary">
+        Edit Review
+      </Button>
+    </div>
+  );
 }
-export default Review
+
+export default Review;
